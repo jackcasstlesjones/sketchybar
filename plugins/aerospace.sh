b8cd605680589e183574f645a969bdf7655e6d94
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-24 11:58:10 +0000

    colours are working, display on main monitor only

--- plugins/aerospace.sh
+++ plugins/aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+  sketchybar --set $NAME background.color=0xffffc100 label.shadow.drawing=off icon.shadow.drawing=off background.border_width=2 icon.color=0xFF000000 label.color=0xff000000
 else
-    sketchybar --set $NAME background.drawing=off
+  sketchybar --set $NAME background.color=0x44FFFFFF label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0 icon.color=0xFFffffff label.color=0xffffffff
 fi
