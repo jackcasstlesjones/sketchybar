4e047b6fea8fbcebc95d9a6e8c615424e383616b
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-26 19:06:05 +0000

    fix media player

--- items/media.sh
+++ items/media.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-sketchybar --add item media right \
+sketchybar --add item media center \
            --set media label.color=$TEXT_COLOR \
                        label.max_chars=20 \
                        icon.padding_left=0 \
                        scroll_texts=on \
                        icon=􀑪             \
-                       icon.color=$TEXT_COLOR  \
+                       icon.color=$TEXT_COLOR   \
                        background.drawing=off \
                        script="$PLUGIN_DIR/media.sh" \
-                       background.colo=$ITEM_BG_COLOR
            --subscribe media media_change
