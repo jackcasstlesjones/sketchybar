237ded0dd46c8d391ce6cab670a9d9671684d943
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-26 19:09:16 +0000

    add placeholder

--- plugins/update_workspace_icons.sh
+++ plugins/update_workspace_icons.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CONFIG_DIR="$HOME/.config/sketchybar"
+DEFAULT_PLACEHOLDER="–"  # You can change this to any icon or text you prefer
+
 
 update_space_icons() {
     local sid=$1
@@ -14,7 +16,7 @@ update_space_icons() {
             icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
         done <<<"${apps}"
     else
-        icon_strip=""
+        icon_strip=" $DEFAULT_PLACEHOLDER"
     fi
     sketchybar --set space.$sid label="$icon_strip"
 }
