5dfd9f865f1c73cfcafe44a66a50d7159cd7468d
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-27 17:40:28 +0000

    floating ui with media

--- items/media.sh
+++ items/media.sh
@@ -8,6 +8,19 @@ sketchybar --add item media center \
                        icon=􀑪             \
                        icon.color=$TEXT_COLOR   \
                        background.drawing=off \
-                    background.color=none \
+                        background.border_color=0xAAffc100 \
+           background.border_width=2 \
+           background.corner_radius=8 \
+           background.height=35 \
+           background.padding_left=20 \
+           icon.padding_left=20 \
+           icon.padding_right=10 \
+           label.padding_right=20 \
+           background.padding_right=200 \
+           padding_left=200 \
+           background.color=0xff353C4B \
+background.border_color=0xAAffc100 \
+           background.border_width=2 \
+           background.corner_radius=8 \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
