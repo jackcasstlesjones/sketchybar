39357354ac0a04b1e8fb3674edc66040985c54b0
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-27 18:06:48 +0000

    add background to calendar

--- items/calendar.sh
+++ items/calendar.sh
@@ -5,10 +5,8 @@ sketchybar --add item calendar right \
                           update_freq=30 \
                           script="$PLUGIN_DIR/calendar.sh" \
                           icon.color=$WHITE \
-                          background.color=none\
-                          label.color=$WHITE
+                          label.color=$WHITE \
                              background.height=35 \
-           icon.padding_left=20 \
-           icon.padding_right=10 \
-           label.padding_right=20 \
            background.color=0xff353C4B \
+             background.border_color=0xAAffc100 \
+           background.border_width=2 \
