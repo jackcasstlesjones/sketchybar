753ea11da3fb6425468d61ef1d63d99a45d84a92
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-28 20:32:11 +0000

    swap music note for spotify logo

--- items/media.sh
+++ items/media.sh
@@ -4,8 +4,9 @@ sketchybar --add item media center \
            --set media label.color=$WHITE \
                        label.max_chars=20 \
                        icon.padding_left=0 \
+                       icon=    \
+           	           icon.font="Hack Nerd Font Mono:regular:30.0" \
                        scroll_texts=on \
-                       icon=􀑪             \
                        icon.color=$TEXT_COLOR   \
                        background.drawing=off \
                         background.border_color=0xAAffc100 \
@@ -21,3 +22,5 @@ background.border_color=0xAAffc100 \
            background.corner_radius=8 \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
+
+                    #    icon=􀑪             \
