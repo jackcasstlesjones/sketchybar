45cbb21dd63c2d184e52a7e940646500a141d2c4
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-29 22:33:19 +0000

    remove media padding

--- items/media.sh
+++ items/media.sh
@@ -11,7 +11,6 @@ sketchybar --add item media right \
                        background.drawing=off \
            background.border_width=2 \
            background.corner_radius=8 \
-           icon.padding_left=20 \
            icon.padding_right=10 \
            label.padding_right=20 \
            background.color=none \
@@ -20,6 +19,7 @@ sketchybar --add item media right \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
 
+        #    icon.padding_left=20 \
         #    background.height=40 \
                         # background.border_color=$BRACKET_BORDER \
                     #    icon=􀑪             \
