21c6bcd6f656c05039cde46847d60cd64c62ab10
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-29 11:20:25 +0000

    move calendar to left hand size

--- items/calendar.sh
+++ items/calendar.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sketchybar --add item calendar right \
+sketchybar --add item calendar left \
            --set calendar icon=􀧞  \
                           update_freq=30 \
                           script="$PLUGIN_DIR/calendar.sh" \
                           icon.color=$WHITE \
                           label.color=$WHITE \
-                             background.height=40 \
                              label.padding_right=20 \
                              icon.padding_left=20 \
            background.color=0xff353C4B \
-             background.border_color=$BRACKET_BORDER \
            background.border_width=2 \
+
+            #  background.border_color=$BRACKET_BORDER \
+                            #  background.height=40 \
