7d1a14db95fe7a1dd121cf65e1dfe75a36653af8
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-01-29 12:42:24 +0000

    add border to active workspace

--- plugins/aerospace.sh
+++ plugins/aerospace.sh
@@ -7,7 +7,7 @@ source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.color=$ITEM_BG_COLOR label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0 icon.color=$TEXT_COLOR label.color=$TEXT_COLOR
+  sketchybar --set $NAME background.color=$ITEM_BG_COLOR label.shadow.drawing=off icon.shadow.drawing=off background.border_width=2 background.border_color=0xffffc100 icon.color=$TEXT_COLOR label.color=$TEXT_COLOR
 else
   sketchybar --set $NAME background.color=0x44FFFFFF label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0 icon.color=0xffffffff label.color=0xffffffff
 fi
